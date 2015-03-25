11fd9ea91867e1df1145d82f128e6d124d85f8bd
TheKeeperOfPie <igpx346@gmail.com>
2015-03-24 21:20:44 -0500

    Start rewrite branch

---
+++ app/src/main/java/cw/kop/autobackground/sources/SortData.java
@@ -0,0 +1,7 @@
+package cw.kop.autobackground.sources;
+
+/**
+ * Created by TheKeeperOfPie on 3/18/2015.
+ */
+public class SortData {
+}
---
+++ app/src/main/java/cw/kop/autobackground/sources/SourceSortSpinnerAdapter.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) Winson Chiu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cw.kop.autobackground.sources;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import cw.kop.autobackground.R;
+import cw.kop.autobackground.settings.AppSettings;
+
+/**
+ * Created by TheKeeperOfPie on 11/10/2014.
+ */
+public class SourceSortSpinnerAdapter extends ArrayAdapter<String> {
+
+    private List<String> itemList;
+    private LayoutInflater inflater;
+    private int backgroundColor;
+
+    public SourceSortSpinnerAdapter(Context context, int resource, List<String> objects) {
+        super(context, resource, objects);
+
+        itemList = objects;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        backgroundColor = context.getResources().getColor(AppSettings.getBackgroundColorResource());
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.spinner_row, parent, false);
+        }
+
+        TextView spinnerText = (TextView) convertView.findViewById(R.id.spinner_text);
+        spinnerText.setText(itemList.get(position));
+        spinnerText.setBackgroundColor(backgroundColor);
+
+        return convertView;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+
+        TextView spinnerText;
+
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.spinner_row, parent, false);
+
+            spinnerText = (TextView) convertView.findViewById(R.id.spinner_text);
+
+            convertView.setTag(new ViewHolder(spinnerText));
+        }
+
+        ViewHolder viewHolder = (ViewHolder) convertView.getTag();
+        spinnerText = viewHolder.spinnerText;
+
+        spinnerText.setText(itemList.get(position));
+        spinnerText.setBackgroundColor(backgroundColor);
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+
+        protected final TextView spinnerText;
+
+        public ViewHolder(TextView spinnerText) {
+            this.spinnerText = spinnerText;
+        }
+    }
+
+}
